a62356b111eb9f21c4392f36bbe54e8c9076219e
Gabry147 <gabriscalen@gmail.com>
2016-03-10 21:25:12 +0100

    CheckPoint activity

--- app/src/main/java/ictsdays16/hackaton/stilemediterraneo/DevCreditsActivity.java
+++ app/src/main/java/ictsdays16/hackaton/stilemediterraneo/DevCreditsActivity.java
@@ -2,6 +2,7 @@ package ictsdays16.hackaton.stilemediterraneo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DevCreditsActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class DevCreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dev_credits);
+    }
+    public void returnHome(View v) {
+        finish();
     }
 }
