9fc0d892e65d16934f7028fab70cfa2908a93470
StefanoTavonatti <stefanotavonatti93@gmail.com>
2016-03-11 01:31:55 +0100

    Buttons home, and icons

--- app/src/main/java/ictsdays16/hackaton/stilemediterraneo/MealMainActivity.java
+++ app/src/main/java/ictsdays16/hackaton/stilemediterraneo/MealMainActivity.java
@@ -56,7 +56,7 @@ public class MealMainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.mangiare);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
