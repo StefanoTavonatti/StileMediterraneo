49b99c8de894d384a3ad79be1ff66a86213567ae
StefanoTavonatti <stefanotavonatti93@gmail.com>
2016-03-11 12:16:12 +0100

    Striiings

--- app/src/main/java/ictsdays16/hackaton/stilemediterraneo/MealMainActivity.java
+++ app/src/main/java/ictsdays16/hackaton/stilemediterraneo/MealMainActivity.java
@@ -72,7 +72,7 @@ public class MealMainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Worck in progress!!!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
